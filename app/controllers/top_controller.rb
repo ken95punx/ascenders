b24759b55c7b31d1396e2aa251cc4f062dabfdcd
ken ishihara <k.ishihara@ascenders.co.jp>
2017-08-17 18:25:02 +0900

    set Latest movies

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,7 +2,7 @@ class TopController < ApplicationController
   def index
   	@movies = Movie.all.order(created_at: :desc)
   	@articles = Article.all.order(created_at: :desc)
-  	@movie = Movie.find_by(id: 1)
+  	@movie = Movie.last
   end
 
   def click_movie
