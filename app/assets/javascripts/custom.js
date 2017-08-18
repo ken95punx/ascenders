976cf2c08837435a92797f12f6d8f25d486b7c18
ken ishihara <k.ishihara@ascenders.co.jp>
2017-08-18 16:02:21 +0900

    style serch box sp

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,5 +1,17 @@
+//footerの高さ指定
 $(function() {
 	var footer_height = $('footer').height();
 
 	$('footer').css('bottom', "-" + footer_height + "px");
+});
+
+// bxslider
+$(function() {
+  $('.movie-slider').bxSlider({
+    slideWidth: 200,
+    minSlides: 2,
+    maxSlides: 3,
+    slideMargin: 10
+  });
+  $('.bx-wrapper').css('max-width', '100%');
 });
