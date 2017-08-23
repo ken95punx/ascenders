55d4fa01b2f00589ac137b5ecbd64ea28646e461
ken ishihara <k.ishihara@ascenders.co.jp>
2017-08-23 20:58:04 +0900

    association movies players model

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,8 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-
-	def player
-		player = Player.find_by(id: self.player_id)
-		return player.name
-	end
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,6 @@
 class Player < ApplicationRecord
+  has_many :movies;
+
   def sport
     sport = Sport.find_by(id: self.sports_id)
     return sport.name
