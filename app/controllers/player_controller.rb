e1c1691f4e266acdd25818055c184054068c9d83
ken ishihara <k.ishihara@ascenders.co.jp>
2017-08-25 20:24:58 +0900

    add strong params player controller

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -4,4 +4,9 @@ class PlayerController < ApplicationController
   def index
     @players = Player.order(created_at: :desc).page(params[:page]).per(PER)
   end
+
+  private
+    def player_params
+      params.require(:player).permit(:name, :kana_name, :profile_image, :cover_image, :place_birth, :blood_type, :league_school, :team, :height, :weight, :alma_mater, :website, :introduction, :opponent_next, :time_next, :url_next, :opponent_result, :result_result, :self_point_result, :enemy_point_result, :url_result, :staff_id, :birth_date, :sport_id)
+    end
 end
