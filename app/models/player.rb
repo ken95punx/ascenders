4ad25fafedb35fc5a83b67f5990e18939c994806
ken ishihara <k.ishihara@ascenders.co.jp>
2017-08-25 19:49:47 +0900

    associate model column uploader

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,4 +2,7 @@ class Player < ApplicationRecord
   has_many :movies
   has_many :articles
   belongs_to :sport
+
+  mount_uploader :profile_image, ImageUploader
+  mount_uploader :cover_image, ImageUploader
 end
