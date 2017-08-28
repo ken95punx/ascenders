cbf5dc52515d9fd813e6189b53e56a3c6b0167ac
ken ishihara <k.ishihara@ascenders.co.jp>
2017-08-28 16:22:15 +0900

    add administrator model

--- app/models/sport.rb
+++ app/models/sport.rb
@@ -1,7 +1,7 @@
 class Sport < ApplicationRecord
   has_many :players
   has_many :movies
-  has_many :players
+  has_many :articles
 
   mount_uploader :image, ImageUploader
 end
---
+++ spec/factories/administrators.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :administrator do
+    name "MyString"
+    login "MyString"
+    password "MyString"
+  end
+end
