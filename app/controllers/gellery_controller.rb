59d2d5250dcc58faf652aaa49083c2eb82f8110e
ken ishihara <k.ishihara@ascenders.co.jp>
2017-08-29 19:55:28 +0900

    create gellery page

--- app/controllers/gellery_controller.rb
+++ app/controllers/gellery_controller.rb
@@ -1,4 +1,13 @@
 class GelleryController < ApplicationController
+  PER = 12
+
   def index
+    @gelleries = Gellery.where(player_id: params[:player_id]).order(created_at: :desc).page(params[:page]).per(PER)
+    @player = Player.find(params[:player_id])
+  end
+
+  def show
+    @player = Player.find(params[:player_id])
+    @gellery = Gellery.find(params[:id])
   end
 end
