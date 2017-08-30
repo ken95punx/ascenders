51c2cc7aca5a2ff56fb7023aacb2c29a13c6c213
ken ishihara <k.ishihara@ascenders.co.jp>
2017-08-30 21:51:20 +0900

    create model friendship

---
+++ db/migrate/20170829114201_create_friend_relationships.rb
@@ -0,0 +1,10 @@
+class CreateFriendRelationships < ActiveRecord::Migration[5.1]
+  def change
+    create_table :friend_relationships do |t|
+      t.integer :follower_id
+      t.integer :followed_id
+
+      t.timestamps
+    end
+  end
+end
