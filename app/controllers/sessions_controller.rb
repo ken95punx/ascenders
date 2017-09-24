5dd498699c047a341b64279080c011aa852ad040
ken ishihara <k.ishihara@ascenders.co.jp>
2017-09-24 20:09:44 +0900

    ログイン・ログアウト処理（コントローラ）

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,21 @@
 class SessionsController < ApplicationController
-  def new
+  def new # ログイン画面
+    @administrator = Administrator.new
   end
 
-  def create
+  def create #ログイン実装
+    if @administrator = login(params[:email], params[:password])
+      session[:administrator_id] = @administrator.id #ログインしたユーザーをセッションユーザーに定義
+      redirect_back_or_to(:administrators, notice: 'ログインに成功しました')
+    else
+      flash.now[:alert] = 'ログインに失敗しました'
+      render action: 'new'
+    end
   end
 
-  def destroy
+  def destroy #ログアウト実装
+    logout
+    session[:administrator_id] = nil #ログアウトしたセッションユーザーを消去
+    redirect_to(login_path, notice: 'ログアウトしました')
   end
 end
