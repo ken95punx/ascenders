0125a285acab24891637a0a3948a62d209a0c059
ken ishihara <k.ishihara@ascenders.co.jp>
2017-09-24 19:59:23 +0900

    管理者用モデルにバリデーション実装（administratorモデル）

--- app/models/administrator.rb
+++ app/models/administrator.rb
@@ -1,5 +1,10 @@
-class Administrator < ApplicationRecord
+class Administrator < ActiveRecord::Base
   has_many :players
-
   authenticates_with_sorcery!
+
+  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
+  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
+
+  validates :email, uniqueness: true
 end
