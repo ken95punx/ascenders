8a75572a142efe702831abfd9b8a7b6585655f33
ken ishihara <k.ishihara@ascenders.co.jp>
2017-09-25 18:29:28 +0900

    認証の導入

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -10,6 +10,10 @@ module Admin
 
     def authenticate_admin
       # TODO Add authentication logic here.
+      current_administrator = Administrator.find_by(id: session[:administrator_id])
+      if current_administrator == nil
+        redirect_to(login_path, notice: 'ログインしてください')
+      end
     end
 
     # Override this value to specify the number of elements to display at a time
