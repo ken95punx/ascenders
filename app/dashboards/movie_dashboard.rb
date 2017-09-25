1818bd8dafed1f6d5dfa3a105a99c67b021082a3
ken ishihara <k.ishihara@ascenders.co.jp>
2017-09-25 18:32:08 +0900

    動画ダッシュボード作成

---
+++ app/dashboards/movie_dashboard.rb
@@ -0,0 +1,63 @@
+require "administrate/base_dashboard"
+
+class MovieDashboard < Administrate::BaseDashboard
+  # ATTRIBUTE_TYPES
+  # a hash that describes the type of each of the model's fields.
+  #
+  # Each different type represents an Administrate::Field object,
+  # which determines how the attribute is displayed
+  # on pages throughout the dashboard.
+  ATTRIBUTE_TYPES = {
+    player: Field::BelongsTo,
+    sport: Field::BelongsTo,
+    id: Field::Number,
+    title: Field::String,
+    created_at: Field::DateTime,
+    updated_at: Field::DateTime,
+    movie_thum: Field::String,
+    movie_html: Field::Text,
+  }.freeze
+
+  # COLLECTION_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's index page.
+  #
+  # By default, it's limited to four items to reduce clutter on index pages.
+  # Feel free to add, remove, or rearrange items.
+  COLLECTION_ATTRIBUTES = [
+    :player,
+    :sport,
+    :id,
+    :title,
+  ].freeze
+
+  # SHOW_PAGE_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's show page.
+  SHOW_PAGE_ATTRIBUTES = [
+    :player,
+    :sport,
+    :id,
+    :title,
+    :created_at,
+    :updated_at,
+    :movie_thum,
+    :movie_html,
+  ].freeze
+
+  # FORM_ATTRIBUTES
+  # an array of attributes that will be displayed
+  # on the model's form (`new` and `edit`) pages.
+  FORM_ATTRIBUTES = [
+    :player,
+    :sport,
+    :title,
+    :movie_thum,
+    :movie_html,
+  ].freeze
+
+  # Overwrite this method to customize how movies are displayed
+  # across all pages of the admin dashboard.
+  #
+  # def display_resource(movie)
+  #   "Movie ##{movie.id}"
+  # end
+end
