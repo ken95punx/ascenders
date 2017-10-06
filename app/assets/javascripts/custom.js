1d2a3aea2d91c429dcf195b2cb4e6a401dae7e5a
ken ishihara <k.ishihara@ascenders.co.jp>
2017-10-06 19:27:49 +0900

    プロフィール分開閉ボタン実装

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -22,3 +22,13 @@ $(function() {
   });
 });
 
+// プロフィールボタン開閉
+$(function() {
+  $("#profile").on("hide.bs.collapse", function(){
+    $(".profile-btn").html('プロフィールを読む<span class="glyphicon glyphicon-plus"></span>');
+  });
+  $("#profile").on("show.bs.collapse", function(){
+    $(".profile-btn").html('閉じる<span class="glyphicon glyphicon-minus"></span>');
+  });
+});
+
